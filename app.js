ba0119add931e404885a1c88f6859fd9992f3ba8
nico <nicolas.escudero00@gmail.com>
2022-06-11 19:40:40 -0300

    adding cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const app=express();
 const morgan=require('morgan');
+const cors=require('cors');
 require('dotenv').config();
 const {dbConnection}=require('./database');
 dbConnection();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use('/',require('./routes'));
 
